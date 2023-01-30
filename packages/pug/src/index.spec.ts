5ee41072bba1900b5dd71ad4e0230be9dc95a76b
Kassuro <l.fuhl@demv.de>
2023-01-30 12:38:19 +0100

    feat(pug): implement pug plugin with TransformPlugin class from core

---
+++ packages/pug/src/index.spec.ts
@@ -0,0 +1,62 @@
+import { readdir, rm } from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+import { extname, resolve } from 'node:path';
+import { describe, it, expect, beforeAll, afterAll } from 'vitest';
+import { ReRelaxed } from '@re-relaxed/core';
+
+import { PugTransformPlugin } from '.';
+
+describe('Pug transform plugin', () => {
+  beforeAll(async () => {
+    await ReRelaxed.getInstance({
+      tmpDir: '.',
+      outDir: '.',
+      plugins: {
+        transform: new PugTransformPlugin(),
+      },
+    });
+  });
+
+  afterAll(async () => {
+    // @todo share such utils across packages somehow
+    const fileList = await readdir('.');
+    const deletePromises: Promise<void>[] = [];
+
+    fileList.forEach((file) => {
+      const fileExtension = extname(file);
+
+      if (fileExtension === '.html' || fileExtension == '.pdf') {
+        deletePromises.push(rm(resolve('.', file), { maxRetries: 2, retryDelay: 100 }));
+      }
+    });
+
+    await Promise.all(deletePromises);
+  });
+
+  describe('', async () => {
+    it('has working extension check for pug files', async () => {
+      const reRelaxed = await ReRelaxed.getInstance();
+
+      expect(reRelaxed.generatePdf(resolve('.', './src/__mock__/template.pug'))).resolves.toBe(
+        undefined,
+      );
+    });
+
+    it('generates a pdf file from template', async () => {
+      const reRelaxed = await ReRelaxed.getInstance();
+
+      await reRelaxed.generatePdf(resolve('.', './src/__mock__/template.pug'));
+
+      let expectedOutputFile = resolve('.', 'template.pdf');
+      expect(existsSync(expectedOutputFile)).toBeTruthy();
+
+      await reRelaxed.generatePdf(resolve('.', './src/__mock__/templateWithData.pug'), {
+        test: 'hello',
+        obj: { test: 1 },
+      });
+
+      expectedOutputFile = resolve('.', 'templateWithData.pdf');
+      expect(existsSync(expectedOutputFile)).toBeTruthy();
+    });
+  });
+});
---
+++ packages/pug/src/index.ts
@@ -0,0 +1,30 @@
+import pug from 'pug';
+import { TransformPlugin } from '@re-relaxed/core';
+
+import { readFile, writeFile } from 'node:fs/promises';
+import { extname } from 'node:path';
+
+export class PugTransformPlugin extends TransformPlugin {
+  async transform(
+    inputPath: string,
+    outputPath: string,
+    templateData?: Record<string, unknown>,
+  ): Promise<string> {
+    const templateEntry = await readFile(inputPath, {
+      encoding: 'utf-8',
+    });
+
+    const renderedHTML = pug.render(templateEntry, {
+      ...templateData,
+    });
+
+    await writeFile(outputPath, renderedHTML, {
+      encoding: 'utf-8',
+    });
+
+    return outputPath;
+  }
+  checkFileType(path: string): boolean {
+    return extname(path) === '.pug';
+  }
+}
