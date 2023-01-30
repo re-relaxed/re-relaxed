f998698cc82344b9776c0238af461a79a1778fe6
Kassuro <l.fuhl@demv.de>
2023-01-30 12:40:31 +0100

    chore(pug): add test script for manual testing pdf generation

---
+++ packages/pug/scripts/pdf-test/index.ts
@@ -0,0 +1,30 @@
+import { resolve } from 'node:path';
+import { ReRelaxed } from '@re-relaxed/core';
+
+import { PugTransformPlugin } from '../../dist/cjs/index';
+
+const main = async () => {
+  const instance = await ReRelaxed.getInstance({
+    /**
+     * Currently we don't have a solution to move file
+     * dependencies with the copied tmp file.
+     * This solves the problem for the moment as the
+     * tmp file is in same dir as original file.
+     */
+
+    tmpDir: __dirname,
+    outDir: __dirname,
+    plugins: {
+      transform: new PugTransformPlugin(),
+    },
+  });
+
+  await instance.generatePdf(resolve(__dirname, './simpleTemplate.pug'));
+  await instance.generatePdf(resolve(__dirname, './templateWithData.pug'), {
+    msg: { text: 'hello world', author: 'some person' },
+  });
+
+  process.exit();
+};
+
+main();
